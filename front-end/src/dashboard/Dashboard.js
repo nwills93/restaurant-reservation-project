5a01fdd806103eb9bdc2090d5ab9fc7ea35c949d
Nathaniel Wills <nathanielwills708@gmail.com>
2022-04-21 14:17:57 -0700

    moved tables table and table rows to their own components

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, Link, useHistory } from "react-router-dom";
-import { listReservations, listTables, deleteTableAssignment, cancelReservation } from "../utils/api";
+import { listReservations, listTables, deleteTableAssignment } from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
 import { today, previous, next } from "../utils/date-time";
 import ReservationTableDisplay from "../layout/ReservationTableDisplay"
+import TablesTableDisplay from "../layout/TablesTableDisplay";
 
 /**
  * Defines the dashboard page.
@@ -42,73 +43,32 @@ function Dashboard() {
     return () => abortController.abort();
   }
 
-  // const handleCancel = (id) => {
-  //   if (window.confirm("Do you want to cancel this reservation? This cannot be undone.")) {
-  //     cancelReservation(id).then(history.go(0))
-  //   }
-  // }
-
-  // const reservationRows = reservations.map((reservation) => (
-  //   <tr key={reservation.reservation_id}>
-  //     <td>{`${reservation.last_name}, ${reservation.first_name}`}</td>
-  //     <td>{reservation.mobile_number}</td>
-  //     <td>{reservation.reservation_time}</td>
-  //     <td>{reservation.people}</td>
-  //     <td data-reservation-id-status={reservation.reservation_id}>{reservation.status}</td>
-  //     {reservation.status === 'booked' && (
+  // const tableRows = tables.map((table) => (
+  //   <tr key={table.table_id}>
+  //     <td>{table.table_id}</td>
+  //     <td>{table.table_name}</td>
+  //     <td>{table.capacity}</td>
+  //     <td data-table-id-status={`${table.table_id}`}>
+  //       {table.reservation_id === null ? "Free" : "Occupied"}
+  //     </td>
+  //     {table.reservation_id ? (
   //       <td>
-  //         <Link to={`/reservations/${reservation.reservation_id}/seat`}>
-  //           <button type="button" className="btn btn-secondary">
-  //             Seat
-  //           </button>
-  //         </Link>
-  //       </td>
-  //     )}
-  //     <td>
-  //       <Link to={`/reservations/${reservation.reservation_id}/edit`}>
-  //         <button type="button" className="btn btn-secondary">
-  //             Edit
+  //         <button
+  //           type="button"
+  //           className="btn btn-secondary"
+  //           data-table-id-finish={table.table_id}
+  //           onClick={() => {
+  //             if (window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
+  //               deleteTableAssignment(table.table_id).then(history.go(0))        
+  //             }
+  //           }}
+  //         >
+  //           Finish
   //         </button>
-  //       </Link>
-  //     </td>
-  //     <td>
-  //       <button 
-  //         type="button" 
-  //         className="btn btn-secondary" 
-  //         data-reservation-id-cancel={reservation.reservation_id}
-  //         onClick={() => handleCancel(reservation.reservation_id)}>
-  //             Cancel
-  //       </button>
-  //     </td>
+  //       </td>
+  //     ) : null}
   //   </tr>
   // ));
-
-  const tableRows = tables.map((table) => (
-    <tr key={table.table_id}>
-      <td>{table.table_id}</td>
-      <td>{table.table_name}</td>
-      <td>{table.capacity}</td>
-      <td data-table-id-status={`${table.table_id}`}>
-        {table.reservation_id === null ? "Free" : "Occupied"}
-      </td>
-      {table.reservation_id ? (
-        <td>
-          <button
-            type="button"
-            className="btn btn-secondary"
-            data-table-id-finish={table.table_id}
-            onClick={() => {
-              if (window.confirm("Is this table ready to seat new guests? This cannot be undone.")) {
-                deleteTableAssignment(table.table_id).then(history.go(0))        
-              }
-            }}
-          >
-            Finish
-          </button>
-        </td>
-      ) : null}
-    </tr>
-  ));
 
   return (
     <main>
@@ -149,20 +109,11 @@ function Dashboard() {
       <div className="d-flex justify-content-between">
         {reservations && (
           <ReservationTableDisplay reservations={reservations} />
+        )}
+        {tables && (
+          <TablesTableDisplay tables={tables}/>
         )}        
-        {/* <table className="table table-bordered border-dark table-hover mr-2">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Mobile Number</th>
-              <th>Reservation Time</th>
-              <th>Number of People in Party</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>{reservationRows}</tbody>
-        </table> */}
-        <table className="table table-bordered border-dark table-hover">
+        {/* <table className="table table-bordered border-dark table-hover">
           <thead>
             <tr>
               <th>#</th>
@@ -172,7 +123,7 @@ function Dashboard() {
             </tr>
           </thead>
           <tbody>{tableRows}</tbody>
-        </table>
+        </table> */}
       </div>
     </main>
   );
